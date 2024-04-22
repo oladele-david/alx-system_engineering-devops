3ee589e878bc29b50163c1d37d36441d9a30eb00
oladele.david <tdboy59@gmail.com>
2024-04-23 00:59:41 +0100

    task 2 fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,8 @@
+#!/usr/bin/pup
 # Installs Flask
-package { 'python3-pip':
-  ensure => installed,
+
+package { 'flask':
+  ensure => '2.1.0',
+  provider => 'pip3'
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
-}
