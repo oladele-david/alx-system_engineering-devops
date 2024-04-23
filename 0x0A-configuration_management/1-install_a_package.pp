8492d606c79ac6be43ebbc9fc27fc456d73ab024
oladele.david <tdboy59@gmail.com>
2024-04-23 01:04:51 +0100

    fix again

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# Manifest that Installs Flask
-package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3'
+#!/usr/bin/puppet
+# Install a specific version of Flask (2.1.0)
+
+package { 'python3-pip':
+  ensure => installed,
 }
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
